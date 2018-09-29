cdd4e719bee664752ea7bc30b3e3c7bd6e02d428
dennisat <info@anel.co>
2018-09-29 08:11:34 +0200

    move to own file

--- tests/scripts/main.test.ts
+++ tests/scripts/main.test.ts
@@ -1,7 +1,7 @@
 declare let jasmine: any, describe: any, expect: any, it: any;
 if (typeof jasmine !== 'undefined') jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
 
-import {DynaJobQueue} from '../../src/index';
+import {DynaJobQueue} from '../../src';
 
 // help: https://facebook.github.io/jest/docs/expect.html
 
